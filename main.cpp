48ecaad284dd82bff1ba71b68214625e68d6091e
Cheng <765128115@qq.com>
2021-10-01 01:04:00 +0800

    leetcode第一题

--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
+//第一题，两数之和
+vector<int> twoSum(vector<int>& nums, int target) {
+    vector<int> res={0,0};
+    for(int i=0;i<nums.size();i++){
+        for(int j=i;j<nums.size();j++){
+            if (nums[i]+nums[j]==target&&i!=j)
+            {
+                res={i,j};
+                break;
+            }
+        }
+    }
+    return res;
+}
+
 int main() {
-    int a;
-    cin>>a;
-    cout << a << endl;
     return 0;
 }
